6991c617919c3379efed6de4b9f18a9c836e4fab
copper280z <rday689@gmail.com>
2024-12-07 20:11:32 -0500

    remove some filtering in anticog impl, add measuring in both directions

--- Software/anticog.py
+++ Software/anticog.py
@@ -40,59 +40,37 @@ def getAcceleration(df,velocity):
     return accel
 
 def init_motor():
-    Lq = 26.0e-6
-    Ld = 19.5e-6
-
-    R = 0.093
-    m.setpar('motor.conf1.Lambda_m', 0.0013)
-    # Ld = 2382e-6
-    # Lq = 3342e-6
-    # R=7.5
-    
-    m.setpar('motor.conf1.N_pp',  7)
-    m.setpar('motor.conf1.Lq', Lq)
-    m.setpar('motor.conf1.Ld', Ld)
-    m.setpar('motor.state1.R', R)
-    
     m.CL_cur( 0 )
     motor.conf1.ridethewave = 1
     time.sleep(0.5)
-    
+       
     motor.conf1.commutationoffset = 0
-    
-    motor.state1.Valpha_offset = 1.0
-    
+       
+    motor.state1.Valpha_offset = 6
+       
     time.sleep(1)
-    
+       
     offset1 = motor.state1.thetaPark_enc
-    
+       
     motor.state1.Valpha_offset = 0
-    
+       
     motor.conf1.commutationoffset = -offset1
     print(f'Commutation offset: {-offset1}')
-    
-    m.CL_cur( 1.0e3 , 1)
-    # time.sleep(1)
-    # m.setpar('s1.hfi_use_lowpass', 0)
-    # m.setpar('s1.hfi_method', 1)
-    
-    # # Ki = 0.01*2*pi
-    # Ki = 750*2*np.pi
-    # hfi_v = 1.0
-    
-    # m.setpar('s1.hfi_maxvel', 1e6)
-    # m.setpar('s1.hfi_gain', Ki)
-    # # m.setpar('s1.hfi_gain_int2', 0.001*2*pi) # 5*2*pi
-    # m.setpar('s1.hfi_gain_int2', 5*2*np.pi) # 5*2*pi
-    # m.setpar('s1.hfi_V', hfi_v)
-    # m.setpar('s1.hfi_dir_int',0)
-    # m.setpar('s1.hfi_contout',0)
-    # m.setpar('motor.state1.Id_offset_SP',0.1)
-    # m.setpar('s1.hfi_on', 1)
-    # m.setpar('c1.anglechoice', 0)
-    # m.setpar('motor.state1.Id_offset_SP',0.0)
-    # m.setpar( 's1.hfi_useforfeedback' , 1)
-    # m.setpar( 'motor.conf1.maxerror' , 1e6)
+       
+    Lq = 3698e-6
+    Ld = 2376e-6
+       
+    R = 6.87
+    m.setpar('motor.conf1.Lambda_m', 0.019)
+    m.setpar('motor.conf1.N_pp',  11)
+    m.setpar('motor.conf1.Lq', Lq)
+    m.setpar('motor.conf1.Ld', Ld)
+    m.setpar('motor.state1.R', R)
+       
+       
+    m.CL_cur( 0.5e3 , 1)
+    m.setpar('motor.conf1.anglechoice', 0)
+
 
 def find_min(start, n_points, direction):
     Iq_test = direction*np.linspace(start,0,n_points)
@@ -127,7 +105,7 @@ def measure(Ic):
     m.setpar('s1.Iq_offset_SP', 0)
     time.sleep(0.2)
     m.setpar('s1.Iq_offset_SP', direction*Imax)
-    time.sleep(0.08)
+    time.sleep(0.2)
     m.setpar('s1.Iq_offset_SP', Ic)
     
     time.sleep(3)
@@ -180,7 +158,6 @@ m.setpar('motor.conf1.cog_ff_gain', 0)
 
 init_motor()
 
-
 m.setTrace(['motor.state1.thetaPark', 
             'motor.state1.encoderPos1',
             'motor.state1.ymech',
@@ -191,12 +168,13 @@ m.setTrace(['motor.state1.thetaPark',
             'motor.state1.Id_meas',
             'motor.state1.Iq_SP',
             'motor.state1.Iq_offset_SP'])
-Imax = 0.9
-Imin_start = 0.2
+Imax = 0.5
+Imin_start = 0.1
 m.setpar('s1.Iq_offset_SP', 0)
 fft_bins = 240
 n_points = 20
-Iq_test = np.linspace(0.4,1.0,10)
+Iq_test = np.linspace(0.05,0.5,20)
+
 
 vels = []
 for i in range(3):
@@ -207,38 +185,42 @@ for i in range(3):
 vel_threshold = np.maximum(0.02,9*np.std(vels))
 
 
-# for I in Iq_test:
-#     Ic_max = I
-#     m.setpar('s1.Iq_offset_SP', Ic_max)
-#     print(f'Iq: {I:.3f}')
-#     time.sleep(0.2)
-#     df = m.trace(0.1)
-#     vel = np.mean(getVelocity(df))
+for I in Iq_test:
+    Ic_max = I
+    m.setpar('motor.state1.Iq_offset_SP',Ic_max)
+    print(f'Iq: {I:.3f}')
+    time.sleep(0.5)
+    df = m.trace(0.1)
+    vel = np.mean(getVelocity(df))
     
-#     if vel > vel_threshold:
-#         #maybe check a couple of times to make sure it's actually spinning?
-#         print('Motor moving!')
-#         print(f'Ic_max = {Ic_max}')
-#         break
+    if vel > vel_threshold:
+        #maybe check a couple of times to make sure it's actually spinning?
+        print('Motor moving!')
+        print(f'Ic_max = {Ic_max}')
+        break
+
+m.setpar('motor.state1.Iq_offset_SP',0)
     
-# Imax = Ic_max
+Imax = Ic_max
 # %%
-fft_bins = 200
+fft_bins = 75
 Icog = np.zeros(4000)
 print('searching for Ic_min')
 
-J =  0.000009
-Km = 0.0013
+J =  0.00002
+Km = 0.019
 
-Ic_min = [0.145]
-Ic_min_n = -0.145
-# Ic_min = find_min(Imin_start, n_points, 1)
-# Ic_min_n = find_min(Imin_start, n_points, -1)
+Ic_min = [0.03013158]
+Ic_min_n = [-0.03013158]
+# Ic_min = [find_min(Imin_start, n_points, 1)]
+# Ic_min_n = [find_min(Imin_start, n_points, -1)]
 # Ic_min = Ic_min + Ic_max/(n_points)
+
 mp = []
 for I in Ic_min:
     mp.append(measure(I))
-m.setpar('s1.Iq_offset_SP', Ic_min[0])
+    
+m.setpar('motor.state1.Iq_offset_SP',Ic_min[0])
 
 plt.figure()
 plt.title('accel')
@@ -250,9 +232,10 @@ meas_p = mp[0]
 
 plt.figure()
 plt.title('Velocity at minimum positive Iq')
-plt.plot(meas_p['angle'], meas_p['velocity'])
-plt.plot(meas_p['angle'], meas_p['filtered velocity'],'r')
-plt.plot(meas_p['angle'], meas_p['accel'],'m')
+plt.plot(meas_p['angle'], meas_p['velocity'], label="velocity")
+plt.plot(meas_p['angle'], meas_p['filtered velocity'],'r', label="filtered velocity")
+plt.plot(meas_p['angle'], meas_p['accel'],'m', label="accel")
+plt.legend()
 plt.show()
 
 Icog = J*meas_p['accel']/Km
@@ -268,4 +251,4 @@ if not np.all(np.isnan(Icog)):
 
     for i,Ic in enumerate(np.roll(Icog,0)):
         m.setparpart('motor.conf1.cog_torque_ff', Ic, i)
-    m.setpar('motor.conf1.cog_ff_gain', -0.9)
+    m.setpar('motor.conf1.cog_ff_gain', -0)
